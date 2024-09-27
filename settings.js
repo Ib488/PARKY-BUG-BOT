3e68afe3c52c01afe10d33550e6aa13e29fde581
Ib488 <Ngatieniska@gmail.com>
2024-09-27 11:14:06 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,15 +11,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "22898133388"
-global.ownername = "🩵 𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛"
+global.ownernomer = "242053793266"
+global.ownername = " 𝑲𝒖𝒓𝒐𝒓𝒐 𝑳𝒖𝒄𝒊𝒇𝒆𝒓 ✞"
 global.ytname = "YT: Jean-parker-tech"
 global.socialm = "GitHub: Jeanparker100"
 global.location = "Togo, maritime, Lomé"
 
-global.ownernumber = '22898133388'  //creator number
+global.ownernumber = '242053793266'  //creator number
 global.ownername = '𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛' //owner name
-global.botname = '𝙿𝙰𝚁𝙺𝚈-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
+global.botname = 'bug of crash ' //name of the bot
 
 //sticker details
 global.packname = '¹⁴³ ᴬˡᵉᵐᵃᵖ 🧸💜'
